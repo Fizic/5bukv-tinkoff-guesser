8250a6271f3bf2b16443bcb7cc858f30c6c0ab8e
Firsov Kirill <kirya.firsov.04@gmail.com>
2022-05-12 20:52:55 +0500

    feat: The solution of the problem

---
+++ README.md
@@ -0,0 +1,20 @@
+# Guesser for https://5bukv.tinkoff.ru/
+
+---
+
+### Quick start
+
+1. Install `python 3.10.*`
+2. Run `git clone https://github.com/Fizic/5bukv_tinkoff_guesser.git`
+3. Run `cd 5bukv_tinkoff_guesser`
+4. Run `python main.py`
+
+---
+
+### User guide
+
+We drive the proposed words into the game.\
+Enter the received values:\
+0 - there is no letter in the cryptic word\
+1 - the letter is in the wrong place\
+2 - the letter is in the word and it is in its place
---
+++ main.py
@@ -0,0 +1,93 @@
+from random import choice
+
+
+def filter_exact_match(words: list, suggested_word: str, answer: str) -> list:
+    for letter_index, status in enumerate(answer):
+        if status != '2':
+            continue
+
+        filtered_words = []
+        for word in words:
+            if word[letter_index] == suggested_word[letter_index]:
+                filtered_words.append(word)
+
+        words = filtered_words.copy()
+        filtered_words.clear()
+
+    return words
+
+
+def filter_mismatch(words: list, suggested_word: str, answer: str) -> list:
+    the_right_words = [True] * len(words)
+    for letter, status in zip(suggested_word, answer):
+        if status != '0':
+            continue
+
+        letter_count = 0
+        for letter_1, status_1 in zip(suggested_word, answer):
+            if letter_1 == letter and status_1 != '0':
+                letter_count += 1
+
+        for word_index, word in enumerate(words):
+            if letter_count < word.count(letter):
+                the_right_words[word_index] = False
+
+    filtered_words = []
+    for status, word in zip(the_right_words, words):
+        if status:
+            filtered_words.append(word)
+
+    return filtered_words.copy()
+
+
+def filter_overlap(words: list, suggested_word: str, answer: str) -> list:
+    for letter_index, status in enumerate(answer):
+        if status != '1':
+            continue
+
+        filtered_words = []
+        for word in words:
+            if word[letter_index] != suggested_word[letter_index]:
+                filtered_words.append(word)
+
+        words = filtered_words.copy()
+        filtered_words.clear()
+
+    the_right_words = [True] * len(words)
+    for letter, status in zip(suggested_word, answer):
+        if status != '1':
+            continue
+
+        for word_index, word in enumerate(words):
+            if letter not in word:
+                the_right_words[word_index] = False
+
+    filtered_words = []
+    for status, word in zip(the_right_words, words):
+        if status:
+            filtered_words.append(word)
+
+    return filtered_words.copy()
+
+
+def filter_words(words: list, suggested_word: str, answer: str) -> list:
+    words = filter_exact_match(words, suggested_word, answer)
+    words = filter_mismatch(words, suggested_word, answer)
+    words = filter_overlap(words, suggested_word, answer)
+    return words
+
+
+def main() -> None:
+    with open('words.txt', 'r', encoding='utf-8') as file:
+        words = file.read().split()
+        while words:
+            suggested_word = choice(words)
+            print(f'Enter the {suggested_word} into the game')
+            answer = input()
+            if answer == '22222':
+                print(f'Congratulations you won')
+            words = filter_words(words, suggested_word, answer)
+
+
+if __name__ == '__main__':
+    main()
